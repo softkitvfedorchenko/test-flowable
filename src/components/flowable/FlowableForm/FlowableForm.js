86140326c2ad3721460f4508b27a2d835717c0d6
softkitvfedorchenko <76946265+softkitvfedorchenko@users.noreply.github.com>
2023-05-05 11:50:54 +0300

    Update FlowableForm.js

--- src/components/flowable/FlowableForm/FlowableForm.js
+++ src/components/flowable/FlowableForm/FlowableForm.js
@@ -13,11 +13,6 @@ import ResearchTeam from "../ResearchTeam/ResearchTeam";
 const FlowableForm = () => {
 	const fetchData = (url, options) => {
 		return fetch(baseUrl + url, options).then(data => ({
-			/**
-			 * on PRODUCTION we use this code (without extra logic):
-			 * json: () => Promise.resolve(data)
-			 *
-			 */
 			json: async () => {
 				// for this url response should be a single value - not an array
 				if (url.includes("person?id_like")) {
